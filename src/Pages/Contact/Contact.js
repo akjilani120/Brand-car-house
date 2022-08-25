d29eb064bceb4b90d419553166ac11e3335cf801
unknown <abdulkaderjilani120@gmail.com>
2022-08-26 00:47:27 +0600

    contact page complete

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faLocationPin, faPhone, } from '@fortawesome/free-solid-svg-icons';
 import "./Contact.css"
-const Contact = () => { 
+const Contact = () => {
     return (
         <div className=''>
             <div className='row'>
@@ -13,32 +13,50 @@ const Contact = () => {
                         <div className='d-flex my-4'>
                             <div><FontAwesomeIcon className='contact-icon  me-2' icon={faEnvelope} /></div>
                             <div className='ms-3'><h4>Chat to us</h4>
-                            <p className='m-0'>Our friendly team is here to help</p>
-                            <p className='m-0'>abdulkaderprofile.com</p>
+                                <p className='m-0'>Our friendly team is here to help</p>
+                                <p className='m-0'>abdulkaderprofile.com</p>
                             </div>
                         </div>
                         <div className='d-flex my-4'>
                             <div><FontAwesomeIcon className='contact-icon  me-2' icon={faLocationPin} /></div>
                             <div className='ms-3'><h4>Office</h4>
-                            <p>Come say hello at our  office HQ</p>
-                            <p className='m-0'>100 Smith Streel</p>
-                            <p className='m-0'>Collingwwod VIC 3066 AU</p>
+                                <p>Come say hello at our  office HQ</p>
+                                <p className='m-0'>100 Smith Streel</p>
+                                <p className='m-0'>Collingwwod VIC 3066 AU</p>
                             </div>
                         </div>
                         <div className='d-flex my-4'>
                             <div><FontAwesomeIcon className='contact-icon  me-2' icon={faPhone} /></div>
                             <div className='ms-3'><h4>Phone</h4>
-                            <p className='m-0'>Mon-Fri From 8am to 5pm</p>
-                            <p className='m-0'>+8801795849068</p>
+                                <p className='m-0'>Mon-Fri From 8am to 5pm</p>
+                                <p className='m-0'>+8801795849068</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-8">
-                    <div className="contract-right p-4">
+                <div className="col-md-8 ">
+                    <div className="contract-right  m-5">
                         <h1 className='my-3'>Level up your brand</h1>
-                        <p>You canreach us anytime via <a href="">hi@untitledui.com</a></p>
-
+                        <p>You can reach us anytime via <a href="">hi@untitledui.com</a></p>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Your Name"/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="you@company.com"/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Phone number</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Your Name"/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlTextarea1" class="form-label">How can we help</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='Tell us a little about the project'></textarea>
+                        </div>
+                        <div class="mb-3">
+                           <button className='btn btn-primary text-center w-100 fw-bold'>Get Started</button>
+                        </div>
                     </div>
                 </div>
             </div>
